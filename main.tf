d026d65252651324ddcaec7927d6af62de16fa86
Lucy Davinhart <lucy@lucymhdavies.com>
2021-11-15 15:40:49 +0000

    Userdata to update webserver config

--- main.tf
+++ main.tf
@@ -110,4 +110,38 @@ resource "aws_instance" "web" {
   lifecycle {
     create_before_destroy = true
   }
+
+  user_data = <<ENDUSERDATA
+cat << EOT > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+    img {
+        width: 35em;
+    }
+</style>
+</head>
+<body>
+<img src="${local.image_url}" />
+<h1>Welcome to nginx</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+EOT
+ENDUSERDATA
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,23 @@ variable "ami_name" {
   default     = "strawbtest/demo/webserver/v0.1.0"
   description = "Which version of the AMI should we use?"
 }
+
+variable "image_type" {
+  type        = string
+  default     = "cat"
+  description = "Type of image to show"
+}
+variable "image_width" {
+  type        = string
+  default     = "560"
+  description = "Width of image"
+}
+variable "image_height" {
+  type        = string
+  default     = "400"
+  description = "Height of image"
+}
+
+locals {
+  image_url = "https://placebear.com/560/400"
+}
