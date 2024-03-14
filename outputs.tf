70166432d75cba5bfe5ecd39c34481593b80c8a9
Lucy Davinhart <lucy@lucymhdavies.com>
2024-03-14 12:10:09 +0000

    fix ec2 instance connect url

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 
 output "ec2_connect_url" {
-  value = "https://${data.aws_region.current.name}.console.aws.amazon.com/ec2/v2/connect/ubuntu/${aws_instance.web.id}"
+  value = "https://${data.aws_region.current.name}.console.aws.amazon.com/ec2/home?region=eu-west-2#ConnectToInstance:instanceId=${aws_instance.web.id}"
 }
 
 output "web_server_ip" {
